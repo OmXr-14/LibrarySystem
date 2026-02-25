f277b6c6bad7d6da8d468371e09d3d6345344505
OmXr <garbero.omar2003@gmail.com>
2026-02-25 15:05:40 +0100

    Initial commit LibrarySystem

---
+++ backend/backend/src/main/java/com/librarysystem/backend/model/Libro.java
@@ -0,0 +1,48 @@
+package com.librarysystem.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "libri")
+public class Libro {
+
+    @Id
+    private String id;
+
+    private String titolo;
+    private String autore;
+    private int annoPubblicazione;
+    private Boolean disponibile;
+
+
+    public Libro(){}
+
+    public Libro(String titolo, String autore, int annoPubblicazione, boolean disponibile){
+        this.titolo = titolo;
+        this.autore = autore;
+        this.annoPubblicazione = annoPubblicazione;
+        this.disponibile = disponibile;
+    }
+
+
+    //Getter
+
+    public String getId(){ return id;}
+    public String getTitolo(){ return titolo;}
+    public String getAutore(){return autore;}
+    public int getAnnoPubblicazione(){return annoPubblicazione;}
+    public Boolean getDisponibile(){return disponibile;}
+
+    //Setter
+
+    public void setId(String id){ this.id = id;}
+    public void setTitolo(String titolo){ this.titolo=titolo;}
+    public void setAutore(String autore){this.autore = autore;}
+    public void setAnnoPubbilcazione(int annoPubblicazione){this.annoPubblicazione = annoPubblicazione;}
+    public void setDisponibile(Boolean disponibile){this.disponibile = disponibile;}
+
+
+    
+
+
+}
---
+++ backend/backend/src/main/java/com/librarysystem/backend/model/Utente.java
@@ -0,0 +1,52 @@
+package com.librarysystem.backend.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+@Document(collection = "utenti")
+public class Utente {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @DocumentReference
+    private List<Libro> libriPrestito = new ArrayList<>();
+
+    public Utente(){};
+
+    public Utente (String name){
+        this.name= name;
+    }
+
+
+
+    //Getter
+    public String getid(){return id;}
+    public String getName(){return name;}
+    public List<Libro> getLibriPrestito(){ return libriPrestito;}
+
+    //Setter
+    public void setId(String id){this.id = id;}
+    public void setName(String name){this.name = name;}
+    public void setLibriPrestito(List<Libro> libriPrestito){this.libriPrestito = libriPrestito;}
+
+
+    //Metodi per i prestiti
+
+    public void prendiInPrestito(Libro libro){
+        this.libriPrestito.add(libro);
+    }
+
+    public void restituisciLibro(Libro libro){
+        
+        this.libriPrestito.removeIf(l->l.getId().equals(libro.getId()));
+    }
+}
+
+
